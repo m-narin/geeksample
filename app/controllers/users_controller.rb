ae6b8706e07c5739a5caa3c2be5d02b6e233a930
mando <y2pgftpe@gmail.com>
2020-11-17 23:47:57 +0900

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @user_tweets = Tweet.where(user_id: @user.id).where("created_at > ?", Time.current.yesterday)
   end
 end
